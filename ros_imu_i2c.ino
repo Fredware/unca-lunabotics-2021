aa7c889f37f9e48c055bbe2cd22acb66af3934bb
Fredware <35937021+Fredware@users.noreply.github.com>
2021-04-13 22:14:51 -0400

    first working version
    - Successfully passed data from sensor to PC
    - Proof of concept. This only works for one channel.
    TODO:
    - change message type from Foat32 to Imu_msg

---
+++ ros_imu_i2c.ino
@@ -0,0 +1,65 @@
+// Basic demo for accelerometer/gyro readings from Adafruit ISM330DHCX
+
+#include <Adafruit_ISM330DHCX.h>
+
+#include <ros.h>
+#include <std_msgs/Float32.h>
+
+Adafruit_ISM330DHCX ism330dhcx;
+
+ros::NodeHandle nh;
+
+std_msgs::Float32 msg;
+ros::Publisher imu_pub("my_imu", &msg);
+
+void setup(void) {
+/*****************************************SERIAL**********************************************/  
+//  Serial.begin(115200);
+//  Serial.println("Adafruit ISM330DHCX test!");
+
+/*****************************************SENSOR**********************************************/  
+  ism330dhcx.begin_I2C();
+
+  ism330dhcx.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+  ism330dhcx.setGyroRange(LSM6DS_GYRO_RANGE_125_DPS);
+  
+  ism330dhcx.setAccelDataRate(LSM6DS_RATE_6_66K_HZ);
+  ism330dhcx.setGyroDataRate(LSM6DS_RATE_6_66K_HZ);
+  
+  ism330dhcx.configInt1(false, false, true); // accelerometer DRDY on INT1
+  ism330dhcx.configInt2(false, true, false); // gyro DRDY on INT2
+
+/*****************************************NODE**********************************************/  
+  nh.initNode();
+  nh.advertise(imu_pub);
+}
+
+long pub_timer;
+
+void loop() {
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+
+  if( millis() > pub_timer){
+   ism330dhcx.getEvent(&accel, &gyro, &temp); 
+   msg.data = accel.acceleration.z;
+   imu_pub.publish(&msg);
+   pub_timer = millis() + 1000;
+  }
+
+  nh.spinOnce();
+
+
+/*****************************************SERIAL**********************************************/  
+//  Serial.print(accel.acceleration.x);
+//  Serial.print(","); Serial.print(accel.acceleration.y);
+//  Serial.print(","); Serial.print(accel.acceleration.z);
+//  Serial.print(",");
+//
+//  Serial.print(gyro.gyro.x);
+//  Serial.print(","); Serial.print(gyro.gyro.y);
+//  Serial.print(","); Serial.print(gyro.gyro.z);
+//  Serial.println();
+//  delay(1000);
+}
